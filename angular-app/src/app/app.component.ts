351dab0e3f2919275f96f5695b907f0c70723298
Madhavi Gunapati <59458995+MadhaviGunapati610@users.noreply.github.com>
2023-10-30 02:49:09 +0000

    angular housing app

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports:[RouterModule],
+  template: `<main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main> `,
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+})
+@Injectable({
+  providedIn: 'root',
 })
 export class AppComponent {
   title = 'angular-app';
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    HousingLocationComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
